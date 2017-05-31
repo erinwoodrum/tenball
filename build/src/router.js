0ed9c5d6cf4fbc17e8d681b9dd04cf67eb25d4b8
Erin Cook <Drazzar@Davids-MacBook-Pro.local>
2017-05-31 15:49:02 -0400

    add signup page.

--- build/src/router.js
+++ build/src/router.js
@@ -6,13 +6,16 @@ router.changePage = function(pageName){
 	if(activeLink) {
 		activeLink.classList.remove('active'); 
 	}
-	document.getElementById('nav-' + pageName).classList.add('active'); 
+	var newActiveLink = document.getElementById('nav-' + pageName); 
+	if(newActiveLink) {
+		newActiveLink.classList.add('active'); 
+	}
 	document.location.hash = pageName;
 }
 
 
 //Onload check the url in order to change the page. 
-var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout']; 
+var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout', 'signup']; 
 var currHash = document.location.hash.replace("#", ""); 
 if(currHash && currHash.length > 0 && pages.indexOf(currHash) > -1){
 	router.changePage(currHash); 
