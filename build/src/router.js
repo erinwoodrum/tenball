bf9687dcdac7e03861eb127491e07b1c2229b629
Erin Cook <Drazzar@Davids-MacBook-Pro.local>
2017-05-31 14:57:51 -0400

    minor changes to image names and rules page.

--- build/src/router.js
+++ build/src/router.js
@@ -14,7 +14,6 @@ router.changePage = function(pageName){
 //Onload check the url in order to change the page. 
 var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout']; 
 var currHash = document.location.hash.replace("#", ""); 
-console.log(currHash); 
 if(currHash && currHash.length > 0 && pages.indexOf(currHash) > -1){
 	router.changePage(currHash); 
 } else {
