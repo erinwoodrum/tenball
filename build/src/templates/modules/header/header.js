90c6bc479d313ca63c1d8bfb13cc17989de10ed1
Erin Cook <Drazzar@Davids-MacBook-Pro.local>
2017-05-30 21:30:07 -0400

    added login buttons, and a footer.

--- build/src/templates/modules/header/header.js
+++ build/src/templates/modules/header/header.js
@@ -1,3 +1,10 @@
-
+//Onload check the url in order to change the page. 
+var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout']; 
+var currHash = document.location.hash.replace("#", ""); 
+if(currHash && currHash.length > 0 && pages.indexOf(currHash) > -1){
+	router.changePage(currHash); 
+} else {
+	document.getElementById('nav-home').classList.add('active'); //home by default. 
+}
 
 
