4ffb9f5dc787beeef07ed5f8763cc85d3924bf76
Erin Cook <Drazzar@Davids-MacBook-Pro.local>
2017-06-01 00:52:20 -0400

    save progress on signups.

--- build/src/router.js
+++ build/src/router.js
@@ -15,11 +15,11 @@ router.changePage = function(pageName){
 
 
 //Onload check the url in order to change the page. 
-var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout', 'signup']; 
+var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout', 'signup', 'currentUser']; 
 var currHash = document.location.hash.replace("#", ""); 
 if(currHash && currHash.length > 0 && pages.indexOf(currHash) > -1){
 	router.changePage(currHash); 
-} else {
+} else { 
 	document.getElementById('nav-home').classList.add('active'); //home by default. 
 }
 
