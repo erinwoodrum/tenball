42806ed2f760cdad87ddc84c24df37edf8c2efa1
Erin Cook <Drazzar@Davids-MacBook-Pro.local>
2017-06-29 22:04:35 -0400

    starting the admin page where we can start tournaments manually.

--- build/src/router.js
+++ build/src/router.js
@@ -19,6 +19,8 @@ router.changePage = function(pageName, queryString){
 
 var stripQueryString = function(currHash){
 	var locOfQuest = currHash.indexOf('?'); 
+	var hash = currHash; 
+	var queryString = ''; 
 	if(locOfQuest > -1){
 		hash = currHash.substr(0, locOfQuest); 
 		queryString = currHash.substr(locOfQuest + 1); 
@@ -28,7 +30,7 @@ var stripQueryString = function(currHash){
 
 //Onload check the url in order to change the page. 
 var pages = ['home', 'tournaments', 'players', 'rules', 'handicap', 'payout', 'signup', 
-	'currentUser', 'ticket', 'login', 'playerDetail']; 
+	'currentUser', 'ticket', 'login', 'playerDetail', 'admin']; 
 var urlComp = stripQueryString(document.location.hash.replace("#", "")); 
 if(urlComp.hash && urlComp.hash.length > 0 && pages.indexOf(urlComp.hash) > -1){
 	router.changePage(urlComp.hash, urlComp.queryString); 
